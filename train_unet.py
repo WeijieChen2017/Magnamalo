745bf7bf31d427562e29ecccd24adf8ec65ef51f
WeijieChen2017 <weijiechen1994@gmail.com>
2021-08-13 10:49:47 -0500

    Alan

--- train_unet.py
+++ train_unet.py
@@ -15,7 +15,7 @@ from tensorflow.keras.optimizers import Adam
 from models import Unet
 from utils import NiftiGenerator
 
-para_name = "ex01"
+para_name = "ex02"
 # Data to be written  
 train_para ={  
     "para_name" : para_name,
